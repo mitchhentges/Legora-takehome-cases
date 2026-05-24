1f91850e3b6c9239d84ba5c8fd03d50bdcad8601
Mitchell Hentges <mitch9654@gmail.com>
2026-05-24 21:29:04 +0200

    Set up chat display tree UI

--- app/ChatsPage.tsx
+++ app/ChatsPage.tsx
@@ -1,12 +1,15 @@
 import { Navigate } from "react-router-dom";
 import {trpcTanstack, trpcReact} from "./trpc.ts";
 import {useQuery, useQueryClient} from "@tanstack/react-query";
-import {useRef} from "react";
 import {useMessageSubscription} from "./messageSubscription.ts";
+import {useState} from "react";
 
 export default () => {
     const chatState = useQuery(trpcTanstack.chatState.queryOptions());
     useMessageSubscription();
+
+    // TODO choose user based on who has most-recent message
+    const [currentPartner, setCurrentPartner] = useState("b@b");
 
     if (chatState.isLoading) return "Loadin' ...";
     if (!chatState.data) {
@@ -15,10 +18,13 @@ export default () => {
 
     return <div>
         Current user: {chatState.data.ownEmail}
-        {Object.entries(chatState.data.chats)?.map(([from, messages]) => (
-            <div key={from}>
-                {messages.map(message => <div key={message.sentAt}>{message.from}: {message.content}</div>)}
+        <div className="flex h-screen w-screen divide-x divide-gray-300 border-t border-gray-300">
+            <div className="w-1/5 divide-y divide-gray-300">
+                {Object.keys(chatState.data.chats).map(fromEmail => <div onClick={() => setCurrentPartner(fromEmail)} key={fromEmail} className={`py-2 hover:bg-gray-100 cursor-pointer ${fromEmail === currentPartner ? "bg-blue-300" : ""}`}>{fromEmail}</div>)}
             </div>
-        ))}
+            <div className="flex-1 divide-y divide-gray-100">
+                {chatState.data.chats[currentPartner].map(message => <div key={message.sentAt}>{message.content}</div>)}
+            </div>
+        </div>
     </div>
 }
